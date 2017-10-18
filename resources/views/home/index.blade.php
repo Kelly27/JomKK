30837f651a0087b9bfb8e2e206dcb36257ad7d92
Kelly Kong <kellykong2772@gmail.com>
2017-10-19 03:59:02 +0800

    done trending with horizontal scroll

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,29 +4,58 @@
 <div class="container home">
     @include('partial.carousel')
     <h1>TRENDING</h1>
-    <div class="row trending">
-        <div class="col-md-4">
-            <div id="home_food" style="background-image: url('images/food1.png')"></div>
-            <div id="info">
-                <p id="trend_title">Bluberry Breakfast</p>
-                <p id="trend_time"><i class="fa fa-clock-o" aria-hidden="true"></i> 2nd Oct, 2017</p>
+    <div class="row vertical-align">
+        <div class="col-xs-1" style="width: 5%">
+            <button id="left-button">
+                <i class="glyphicon glyphicon-chevron-left"></i>
+            </button>
+        </div>
+        <div class="col-xs-10" style="width: 90%">
+            <div class="trending" id="content">
+                <div class="internal">
+                    <div id="home_food" style="background-image: url('images/food1.png')"></div>
+                    <div id="info">
+                        <p id="trend_title">Bluberry Breakfast</p>
+                        <p id="trend_time"><i class="fa fa-clock-o" aria-hidden="true"></i> 2nd Oct, 2017</p>
+                    </div>
+                </div>
+                <div class="internal">
+                    <div id="home_food" style="background-image: url('images/food1.png')"></div>
+                    <div id="info">
+                        <p id="trend_title">Bluberry Breakfast</p>
+                        <p id="trend_time"><i class="fa fa-clock-o" aria-hidden="true"></i> 2nd Oct, 2017</p>
+                    </div>
+                </div>
+                <div class="internal">
+                    <div id="home_food" style="background-image: url('images/food1.png')"></div>
+                    <div id="info">
+                        <p id="trend_title">Bluberry Breakfast</p>
+                        <p id="trend_time"><i class="fa fa-clock-o" aria-hidden="true"></i> 2nd Oct, 2017</p>
+                    </div>
+                </div>
+                <div class="internal">
+                    <div id="home_food" style="background-image: url('images/food1.png')"></div>
+                    <div id="info">
+                        <p id="trend_title">Bluberry Breakfast</p>
+                        <p id="trend_time"><i class="fa fa-clock-o" aria-hidden="true"></i> 2nd Oct, 2017</p>
+                    </div>
+                </div>
+                <div class="internal">
+                    <div id="home_food" style="background-image: url('images/food1.png')"></div>
+                    <div id="info">
+                        <p id="trend_title">Bluberry Breakfast</p>
+                        <p id="trend_time"><i class="fa fa-clock-o" aria-hidden="true"></i> 2nd Oct, 2017</p>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="col-md-4">
-            <div id="home_food" style="background-image: url('images/food2.png')"></div>
-            <div id="info">
-                <p id="trend_title">Bluberry Breakfast</p>
-                <p id="trend_time"><i class="fa fa-clock-o" aria-hidden="true"></i> 2nd Oct, 2017</p>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div id="home_food" style="background-image: url('images/food1.png')"></div>
-            <div id="info">
-                <p id="trend_title">Bluberry Breakfast</p>
-                <p id="trend_time"><i class="fa fa-clock-o" aria-hidden="true"></i> 2nd Oct, 2017</p>
-            </div>
+        <div class="col-xs-1" style="width: 5%">
+            <button id="right-button" style="float: right;">
+                <i class="glyphicon glyphicon-chevron-right"></i>
+            </button>
         </div>
     </div>
+
 </div>
 @endsection
 
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,7 +8,7 @@
     {{-- styles --}}
     <link href="css/app.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="{{asset('css/layout.css')}}">
-    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{asset('font-awesome/css/font-awesome.min.css')}}">
     @yield('style')
     {{-- scripts --}}
     <script src="{{asset('js/app.js')}}"></script>
@@ -17,5 +17,21 @@
     @include('partial.header')
     @yield('content')
     @include('partial.footer')
+    <script data-require="jquery" data-semver="2.1.1" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script type="text/javascript">
+        $('#right-button').click(function() {
+            event.preventDefault();
+            $('#content').animate({
+                scrollLeft: "+=200px"
+            }, "slow");
+        });
+
+        $('#left-button').click(function() {
+          event.preventDefault();
+          $('#content').animate({
+            scrollLeft: "-=200px"
+        }, "slow");
+      });
+  </script>
 </body>
 </html>
