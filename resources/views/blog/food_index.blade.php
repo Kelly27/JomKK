9b8de3d46ba4647db7e7b6da96b3c2e43954130a
Kelly Kong <kellykong2772@gmail.com>
2017-10-20 00:53:58 +0800

    done food index with filters and without pagination - still need to think on how to do pagination

--- resources/views/blog/food_index.blade.php
+++ resources/views/blog/food_index.blade.php
@@ -3,11 +3,35 @@
 @section('content')
 <div class="container blog">
     <div class="row">
-        <div class="col-md-2"></div>
-        <div class="col-md-10">
+        <div class="col-xs-2 filter" >
+            <h2>Filter</h2>
+            @include('blog/partial/food_filter')
+        </div>
+        <div class="col-xs-10">
             @include('partial.breadcrumb', ['breadcrumb' => [['name' => 'Home', 'link' => '/']],
                                                 'active_bread' => 'Food'])
-            <h1>Food</h1>
+            <h1>FOOD</h1>
+            <div class="row">
+                @for($i = 0; $i < 15; $i++)
+                <div class="col-sm-4">
+                    @if($i%2 == 0)
+                    <a href="#" style="text-decoration: none"><div class="item_card" style="background-image: url({{asset('images/food1.png')}});">
+                        <div id="overlay">
+                            <p class="text-center title">Amazing Breakfast at IMAGO</p>
+                            <p class="text-center time"><i class="fa fa-clock-o" aria-hidden="true"></i> 2nd Oct, 2017</p>
+                        </div>
+                    </div></a>
+                    @else
+                    <a href="#" style="text-decoration: none"><div class="item_card" style="background-image: url({{asset('images/food2.png')}});">
+                        <div id="overlay">
+                            <p class="text-center title">Enjoy the Sea View at Suria Food Court</p>
+                            <p class="text-center time"><i class="fa fa-clock-o" aria-hidden="true"></i> 2nd Oct, 2017</p>
+                        </div>
+                    </div></a>
+                    @endif
+                </div>
+                @endfor
+            </div>
         </div>
     </div>
 </div>
---
+++ resources/views/blog/partial/food_filter.blade.php
@@ -0,0 +1,48 @@
+<h3>by Area</h3>
+<form class="area">
+    <input type="checkbox" name="kk_city_center" id="cbox"><label for="cbox"></label> KK City Center <br>
+    <input type="checkbox" name="damai" id="cbox2"><label for="cbox2"></label> Damai <br>
+    <input type="checkbox" name="foh_sang" id="cbox3"><label for="cbox3"></label> Foh Sang <br>
+    <input type="checkbox" name="kelombong" id="cbox4"><label for="cbox4"></label> Kelombong <br>
+    <input type="checkbox" name="likas" id="cbox5"><label for="cbox5"></label> Likas <br>
+    <input type="checkbox" name="lintas" id="cbox6"><label for="cbox6"></label> Lintas <br>
+    <input type="checkbox" name="lok_kawi" id="cbox7"><label for="cbox7"></label> Lok Kawi <br>
+    <input type="checkbox" name="menggatal" id="cbox8"><label for="cbox8"></label> Menggatal <br>
+    <input type="checkbox" name="penampang" id="cbox9"><label for="cbox9"></label> Penampang <br>
+    <input type="checkbox" name="putatan" id="cbox10"><label for="cbox10"></label> Putatan <br>
+    <input type="checkbox" name="tamparuli" id="cbox11"><label for="cbox11"></label> Tamparuli <br>
+    <input type="checkbox" name="tanjung_aru" id="cbox12"><label for="cbox12"></label> Tanjung Aru <br>
+    <input type="checkbox" name="tanjung_lipat" id="cbox13"><label for="cbox13"></label> Tanjung Lipat <br>
+    <input type="checkbox" name="telipok" id="cbox14"><label for="cbox14"></label> Telipok <br>
+    <input type="checkbox" name="tuaran" id="cbox15"><label for="cbox15"></label> Tuaran <br>
+    <input type="checkbox" name="others" id="cbox16"><label for="cbox16"></label> Others <br>
+</form>
+
+<h3>by Cuisine</h3>
+<form>
+    <input type="checkbox" name="chinese" id="cbox17"><label for="cbox17"></label> Chinese <br>
+    <input type="checkbox" name="malay" id="cbox18"><label for="cbox18"></label> Malay <br>
+    <input type="checkbox" name="japanese" id="cbox19"><label for="cbox19"></label> Japanese <br>
+    <input type="checkbox" name="koeran" id="cbox20"><label for="cbox20"></label> Koeran <br>
+    <input type="checkbox" name="indian" id="cbox21"><label for="cbox21"></label> Indian <br>
+    <input type="checkbox" name="Western" id="cbox22"><label for="cbox22"></label> Western <br>
+</form>
+
+<h3>by Meal Type</h3>
+<form>
+    <input type="checkbox" name="breakfast" id="cbox23"><label for="cbox23"></label> Breakfast <br>
+    <input type="checkbox" name="lunch" id="cbox24"><label for="cbox24"></label> Lunch <br>
+    <input type="checkbox" name="tea-break" id="cbox25"><label for="cbox25"></label> Tea Break <br>
+    <input type="checkbox" name="dinner" id="cbox26"><label for="cbox26"></label> Dinner <br>
+    <input type="checkbox" name="supper" id="cbox27"><label for="cbox27"></label> Supper <br>
+</form>
+
+<h3>by Food Serve</h3>
+<form>
+    <input type="checkbox" name="halal" id="cbox28"><label for="cbox28"></label> Halal <br>
+    <input type="checkbox" name="non-halal" id="cbox29"><label for="cbox29"></label> Non-Halal <br>
+    <input type="checkbox" name="vegetarian" id="cbox30"><label for="cbox30"></label> Vegetarian <br>
+    <input type="checkbox" name="no-pork" id="cbox31"><label for="cbox31"></label> No Pork <br>
+    <input type="checkbox" name="pork" id="cbox32"><label for="cbox32"></label> Pork <br>
+</form>
+
